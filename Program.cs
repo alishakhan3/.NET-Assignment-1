5ab418ed14b4ec8172b6278e5b3fea9f8182d2ec
alishakhan3 <alisha.khan@accolitedigital.com>
2024-04-05 11:47:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = add(a, b);
+
+            Console.WriteLine($"Sum: {a} + {b} = {sum}");
+        }
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
